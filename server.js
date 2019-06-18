f495b1222487bb70e51c8a05d346d9a5c538bf29
FizzyMo <carisa1000@yahoo.com>
2019-06-17 19:13:32 -0700

    finished the questions

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("app"));
 
 
 require("./app/routing/apiRoutes")(app);
