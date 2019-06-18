709c71ba5a4b739abc416e0818c35df1a2325649
FizzyMo <carisa1000@yahoo.com>
2019-06-17 20:22:14 -0700

    herok

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,30 +1,15 @@
-
-
-
 var path = require("path");
 
-
-
 module.exports = function(app){
-
     
-
-
+    //root
     app.get("/", function(req, res){
-
         res.sendFile(path.join(__dirname, "../public/home.html"));
-
     });
 
-
-
-
+    //survey route
     app.get("/survey", function(req, res){
-
         res.sendFile(path.join(__dirname, "../public/survey.html"));
-
     });
-
-
 
 }
--- server.js
+++ server.js
@@ -1,23 +1,16 @@
-
+var path = require("path")
 var express = require("express");
-var path = require("path");
 
-
-
-var app = express();
-
-var PORT = process.env.PORT || 3000;
-
+var app = express(); 
+var PORT = process.env.PORT || 3000; 
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("app"))
 
-
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT)
+})
